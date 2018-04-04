0cfb9c0f63620cfdb64a46e37c3986413e5e75fa
xp <xp@CETYSDAM025>
2018-04-04 17:08:41 +0200

    Optimizarlo

--- src/algoritmosordenacion/AlgoritmosOrdenacion.java
+++ src/algoritmosordenacion/AlgoritmosOrdenacion.java
@@ -15,15 +15,18 @@ public class AlgoritmosOrdenacion {
 
     public void ordenacionBurbuja(int[] numeros) {
         int aux; //variable para hacer los intercambios
-        for (int j = 0; j < numeros.length; j++) {
-            for (int i = 0; i < numeros.length - 1; i++) {
+        boolean recolocar = true;
+        while(recolocar){
+            recolocar = false;
+            for (int i = 0; i < numeros.length-1; i++) {
                 if (numeros[i] > numeros[i + 1]) { //intercambio los valores
                     aux = numeros[i + 1];
                     numeros[i + 1] = numeros[i];
                     numeros[i] = aux;
+                    recolocar = true;
+                    }
                 }
-            }
-            System.out.println(Arrays.toString(numeros));
+          System.out.println(Arrays.toString(numeros));
         }
     }
 
@@ -33,7 +36,8 @@ public class AlgoritmosOrdenacion {
     public static void main(String[] args) {
         AlgoritmosOrdenacion ejercicios = new AlgoritmosOrdenacion();
         int[] numeros = {57, 22, 11, 13, 21, 7};
-        ejercicios.ordenacionBurbuja(numeros);
-
+        ejercicios.ordenacionBurbuja(numeros); 
+        //Comprobar que pasa si lo metes en orden y hay que optimizarlo
+        //Que no compruebe siempre el bucle interno todos los numeros
     }
 }
