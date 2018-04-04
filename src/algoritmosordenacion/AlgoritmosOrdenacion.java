66c79836338cff2bc395c7a63e383021c19fe315
xp <xp@CETYSDAM025>
2018-04-04 16:25:04 +0200

    Ordenacion completada

--- src/algoritmosordenacion/AlgoritmosOrdenacion.java
+++ src/algoritmosordenacion/AlgoritmosOrdenacion.java
@@ -13,26 +13,27 @@ import java.util.Arrays;
  */
 public class AlgoritmosOrdenacion {
 
-    public void ordenacionBurbuja(int[] numeros){
+    public void ordenacionBurbuja(int[] numeros) {
         int aux; //variable para hacer los intercambios
-        
-        for(int i=0; i<numeros.length-1; i++){
-            if(numeros[i] > numeros [i+1]){ //intercambio los valores
-                aux = numeros[i+1];
-                numeros [i+1] = numeros[i];
-                numeros[i] = aux;
+        for (int j = 0; j < numeros.length; j++) {
+            for (int i = 0; i < numeros.length - 1; i++) {
+                if (numeros[i] > numeros[i + 1]) { //intercambio los valores
+                    aux = numeros[i + 1];
+                    numeros[i + 1] = numeros[i];
+                    numeros[i] = aux;
+                }
             }
+            System.out.println(Arrays.toString(numeros));
         }
     }
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         AlgoritmosOrdenacion ejercicios = new AlgoritmosOrdenacion();
-        int [] numeros = {57, 22, 11, 13, 21, 7};
+        int[] numeros = {57, 22, 11, 13, 21, 7};
         ejercicios.ordenacionBurbuja(numeros);
-        System.out.println(Arrays.toString(numeros));
+
     }
-    
 }
